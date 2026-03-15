7ef8e427dfede2efd5d38545c534043e3798891c
Thays Lira <thay.lira2026@gmail.com>
2026-03-14 23:05:10 -0300

    feat: adicionando respostas do questionário defeat: adicionando questionário de OO

---
+++ src/br/com/fiap/Questionário.java
@@ -0,0 +1,23 @@
+package br.com.fiap;
+
+        public class Questionário {
+
+            public static void main(String[] args) {
+                System.out.println("1. Orientação a Objetos: É um modelo de programação baseado em 'objetos' que interagem entre si, imitando o mundo real.");
+
+                System.out.println("2. Objeto: É uma instância de uma classe. Algo que existe na memória e tem características e ações.");
+
+                System.out.println("3. Classe: É o 'molde' ou planta baixa que define como os objetos devem ser criados.");
+
+                System.out.println("4. Abstração: É esconder os detalhes complexos e mostrar apenas o que é essencial para quem usa o objeto.");
+
+                System.out.println("5. Herança: É quando uma classe nova aproveita características e comportamentos de uma classe já existente.");
+
+                System.out.println("6. Encapsulamento: É proteger os dados de uma classe, escondendo a sua lógica interna por trás de métodos seguros.");
+
+                System.out.println("7. Polimorfismo: É a capacidade de um mesmo método se comportar de formas diferentes dependendo do objeto.");
+
+                System.out.println("8. Tipos de Polimorfismo: Sobrecarga (mesmo nome, parâmetros diferentes) e Sobreposição (filho altera o método do pai).");
+            }
+        }
+
