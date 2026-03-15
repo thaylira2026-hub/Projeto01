7109c9573da37e413768883185c08773b06cc587
Thays Lira <thay.lira2026@gmail.com>
2026-03-15 01:02:55 -0300

    Adicionando exercicio de Operadores

---
+++ src/br/com/fiap/Operadores.java
@@ -0,0 +1,18 @@
+package br.com.fiap;
+
+    public class Operadores {
+        public static void main(String[] args) {
+            int n1 = 10;
+            double n2 = 2.5;
+
+            double soma = n1 + n2;
+            double sub = n1 - n2;
+            double multi = n1 * n2;
+            double div = n1 / n2;
+
+            System.out.println("Soma: " + soma);
+            System.out.println("Subtração: " + sub);
+            System.out.println("Multiplicação: " + multi);
+            System.out.println("Divisão: " + div);
+    }
+}
